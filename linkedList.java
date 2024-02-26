e40097cff7b4ce8c842c79afb2fccc688526dc8d
mahskas-ramot <sakshamtomar2014@gmail.com>
2024-02-27 02:42:54 +0530

    added string builder

--- linkedList.java
+++ linkedList.java
@@ -18,17 +18,32 @@ class MylinkedList{
 
     public void insert(int value){
         Node node = new Node(value);
-
+        //first create a new node with next always null
+        //then link it to the next node
+        //always two conditions
         if(head == null){
+            //means linked list is empty
             head = node;
         }
         else{
+            //for iteration purposes
             Node current = head;
+            //move the current till the last node is encountered
             while(current.next != null){
+                //while not null move a step forward until the last node is encountered
                 current = current.next;
             }
             current.next = node;
         }
         
+    }
+    public String toString(){
+        Node current = head;
+        StringBuilder sb = new StringBuilder();
+        while(current.next != null){
+            sb.append(current).append("--->");
+            current = current.next;
+        }
+        return sb.toString();
     }
 }
