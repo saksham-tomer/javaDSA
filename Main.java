e89f503de7a7f124843f199ff162cd4a5a948e5d
mahskas-ramot <sakshamtomar2014@gmail.com>
2024-03-08 15:09:14 +0530

    ✌

--- Main.java
+++ Main.java
@@ -1,11 +1,11 @@
-// public class Main {
-//     public static void main(String[] args){
-//         MylinkedList mylinkedList = new MylinkedList();
-//         mylinkedList.insert(1);
-//         mylinkedList.insert(2);
-//         mylinkedList.insert(5);
-//         mylinkedList.insert(3);
-//         System.out.println(mylinkedList.toString());
-//     }
+public class Main {
+    public static void main(String[] args){
+        MylinkedList mylinkedList = new MylinkedList();
+        mylinkedList.insert(1);
+        mylinkedList.insert(2);
+        mylinkedList.insert(5);
+        mylinkedList.insert(3);
+        System.out.println(mylinkedList.toString());
+    }
     
-// }
+}
